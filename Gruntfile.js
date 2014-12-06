c4f4b89a8edfba53e6e48b27a0f6a57353a3b449
bootflat <info@flathemes.com>
2014-12-07 02:12:13 +0800

    support sass 3.4.9, modify gruntfile.js and package.json, min html

--- Gruntfile.js
+++ Gruntfile.js
@@ -114,7 +114,122 @@ module.exports = function(grunt) {
         src: '*.html'
       }
     },
-    sed: {
+    htmlmin: {
+        dist: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeEmptyAttributes: true,
+                removeCommentsFromCDATA: true,
+                removeRedundantAttributes: true,
+                collapseBooleanAttributes: true
+            },
+            files: {
+                // Destination : Source
+                './htmlmin/index.html': './index.html',
+                './htmlmin/getting-started.html': './getting-started.html',
+                './htmlmin/free-psd.html': './free-psd.html',
+                './htmlmin/documentation.html': './documentation.html',
+                './htmlmin/color-picker.html': './color-picker.html',
+                './htmlmin/color-picker-blue.html': './color-picker-blue.html',
+                './htmlmin/color-picker-gray.html': './color-picker-gray.html',
+                './htmlmin/color-picker-green.html': './color-picker-green.html',
+                './htmlmin/color-picker-pink.html': './color-picker-pink.html',
+                './htmlmin/color-picker-purple.html': './color-picker-purple.html',
+                './htmlmin/color-picker-red.html': './color-picker-red.html',
+                './htmlmin/color-picker-yellow.html': './color-picker-yellow.html'
+            }
+        }
+    },
+    jshint: {
+            /*
+                Note:
+                In case there is a /release/ directory found, we don't want to lint that
+                so we use the ! (bang) operator to ignore the specified directory
+            */
+            files: ['Gruntfile.js', 'js/application.js'],
+            options: {
+                curly:   true,
+                eqeqeq:  true,
+                immed:   true,
+                latedef: true,
+                newcap:  true,
+                noarg:   true,
+                sub:     true,
+                undef:   true,
+                boss:    true,
+                eqnull:  true,
+                browser: true,
+
+                globals: {
+                    // AMD
+                    module:     true,
+                    require:    true,
+                    requirejs:  true,
+                    define:     true,
+
+                    // Environments
+                    console:    true,
+
+                    // General Purpose Libraries
+                    $:          true,
+                    jQuery:     true,
+
+                    // Testing
+                    sinon:      true,
+                    describe:   true,
+                    it:         true,
+                    expect:     true,
+                    beforeEach: true,
+                    afterEach:  true
+                }
+            }
+    },
+    imagemin: {
+        png: {
+            options: {
+                optimizationLevel: 7
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: './img/',
+                    src: ['**/*.png'],
+                    dest: './img/compressed/',
+                    ext: '.png'
+                }
+            ]
+        },
+        gif: {
+            options: {
+                optimizationLevel: 3
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: './img/',
+                    src: ['**/*.gif'],
+                    dest: './img/compressed/',
+                    ext: '.gif'
+                }
+            ]
+        },
+        jpg: {
+            options: {
+                progressive: true
+            },
+            files: [
+                {
+                    expand: true,
+                    cwd: './img/',
+                    src: ['**/*.jpg'],
+                    dest: './img/compressed/',
+                    ext: '.jpg'
+                }
+            ]
+        }
+    },
+   sed: {
       versionNumber: {
         pattern: (function () {
           var old = grunt.option('oldver');
@@ -131,11 +246,16 @@ module.exports = function(grunt) {
   require('time-grunt')(grunt);
 
   grunt.registerTask('task-css', ['sass', 'csscomb', 'cssmin']);
+  grunt.registerTask('task-html', ['htmlmin']);
   grunt.registerTask('task-js', ['uglify']);
-  grunt.registerTask('task', ['clean', 'task-css', 'task-js']);
+  grunt.registerTask('task-imagemin', ['imagemin']);
+  grunt.registerTask('task', ['clean', 'task-css', 'task-js','task-imagemin']);
   grunt.registerTask('build', ['task']);
   grunt.registerTask('default', ['task']);
-  grunt.registerTask('test', ['task', 'csslint', 'validation']);
+  grunt.registerTask('check-call', ['csslint', 'validation', 'jshint']);
+  grunt.registerTask('check-css', ['csslint']);
+  grunt.registerTask('check-html', ['validation']);
+  grunt.registerTask('check-js', ['jshint']);
 
   // Version numbering task.
   // grunt change-version-number --oldver=A.B.C --newver=X.Y.Z
